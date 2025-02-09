6317d93b659fe2cc7cb11bb27dc6d3e84ff78b4b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2025-02-09 13:21:24 +0500

    chore: add NoArgsConstructor for LabelCreateDTO for tests

--- src/main/java/hexlet/code/dto/label/LabelCreateDTO.java
+++ src/main/java/hexlet/code/dto/label/LabelCreateDTO.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LabelCreateDTO {
     @NotNull
     @Size(min = 3, max = 1000)
